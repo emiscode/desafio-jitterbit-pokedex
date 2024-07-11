993fbfd39deaa69db02702cc67ec857dcfbaeb2e
Emilio Scodeler <emiscode@gmail.com>
2024-07-11 10:05:59 -0300

    add environment URL

--- app/src/app/pokemon/pokemon.service.ts
+++ app/src/app/pokemon/pokemon.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class PokemonService {
-  private baseUrl = 'http://localhost:3000/api/pokemon';
+  private baseUrl = environment.baseUrl;
 
   constructor(private http: HttpClient) {}
 
